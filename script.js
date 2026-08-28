5d378a43ac73e03a4d91fe8307d74b49a88f20bd
thanuk-sys <thanuksai@gmail.com>
2026-08-28 19:24:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function appendValue(value){
+document.getElementById("display").value += value;
+}
+
+function clearDisplay(){
+document.getElementById("display").value = "";
+}
+
+function deleteLast(){
+let current = document.getElementById("display").value;
+if(current.endsWith("00")){
+document.getElementById("display").value = current.slice(0,-2);
+} else {
+document.getElementById("display").value = current.slice(0,-1);
+}
+}
+
+function calculate(){
+try{
+let result = eval(document.getElementById("display").value);
+document.getElementById("display").value = result;
+}
+catch{
+alert("Invalid Expression");
+}
+}
